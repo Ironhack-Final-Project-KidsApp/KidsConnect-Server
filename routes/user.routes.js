2f400630eb9dd4955f6e90594dda7fbce2744d5b
Inês Almeida <ines_almeida_203@hotmail.com>
2023-09-02 12:03:23 +0100

    favorites

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -65,28 +65,25 @@ router.get('/user/:id/favorites', async (req, res, next) => {
 });
 
 //add or delete favorites route
-router.post('/user/:id/addfavorite/:activityId', isAuthenticated, async (req, res, next) => {
+router.post('/activity/:id/addfavorite', isAuthenticated, async (req, res, next) => {
   try {
-    const userId = req.params.id;
-    const user = await User.findById(userId);
-  
-    const activityId = req.params.activityId; 
-    /* const activity = await User.find({_id: userId, favorite: activityId}); */
+    const userId = req.payload._id; 
+    const activityId = req.params.id;
 
-    if (user.favorite.includes(activityId)) {
-      user.favorite.pull(activityId);
-      await user.save();
-      res.status(200).json({ message: 'Removed from favorites' });
-    } else {
-      user.favorite.push(activityId);
-      await user.save();
-      res.status(200).json({ message: 'Marked as favorite' });
+    const user = await User.findById(userId);
+
+    if (user.favoriteActivities.includes(activityId)) {
+      return res.status(400).json({ message: 'Activity is already a favorite' });
     }
-  } catch (err) {
-    console.error(err);
+
+    user.favoriteActivities.push(activityId);
+    await user.save();
+
+    res.status(201).json({ message: 'Activity marked as favorite' });
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 module.exports = router;
